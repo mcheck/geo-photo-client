357cecafa52ed4ef9d9bc821a28517abde47bc8c
mcheck <mcheck@sologroup.com>
2012-08-06 14:09:17 -0500

    Initial Commit

---
+++ photo-client/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  photo-client
+//
+//  Created by Michael Check on 8/6/12.
+//  Copyright (c) 2012 Solo Group, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ photo-client/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  photo-client
+//
+//  Created by Michael Check on 8/6/12.
+//  Copyright (c) 2012 Solo Group, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
